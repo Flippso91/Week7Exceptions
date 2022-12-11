81137e3bf380aad45864f2a0f0b5a67095f1e19b
Felipe <g00351833@atu.ie>
2022-12-11 01:32:37 +0000

    making it loop until parameters are met

--- src/main/java/org/example/TryCatch.java
+++ src/main/java/org/example/TryCatch.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 
 public class TryCatch {
     public static void main(String[] args) {
+        int myNum = 0;
+        while (myNum == 0) {
 
-        try {
-            System.out.println("Please enter a number between 0 - 9");
-            Scanner myscan = new Scanner(System.in);
-            int myNum = myscan.nextInt();
+            try {
+                System.out.println("Please enter a number between 0 - 9");
+                Scanner myscan = new Scanner(System.in);
+                myNum = myscan.nextInt();
 
-            if (myNum <= 9 && myNum > 0) {
-                System.out.println("You entered " + myNum);
-            } else {
-                System.out.println("Not a valid number");
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("You entered " + myNum);
+                } else {
+                    System.out.println("Not a valid number");
+                    myNum = 0;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Not a number, please enter a valid numner");
             }
-        }
-        catch (InputMismatchException e){
-            System.out.println("Not a number, please enter a valid numner");
         }
     }
 }
